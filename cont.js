b30ee5826e420850a0856f060984e22500f224b1
AnkurSainiAns <an2811saini@gmail.com>
2025-08-19 09:21:14 +0530

    new update

---
+++ cont.js
@@ -0,0 +1,17 @@
+let contact = document.getElementById("contact")
+contact.innerHTML = `
+    <address>
+        <h4>Address</h4>
+        <p>Saini Samaj Dhramshala,</p>
+        <p>Rajpura, Pilani-333031</p>
+        <p>Rajasthan, India</p>
+    </address>
+
+    <div id="contact-info">
+        <h4>Contact Information</h4>
+        <p>Email: <a href="mailto:an2811saini@gmail.com">an2811saini@gmail.com</a></p>
+        <p>Phone: <a href="tel:8824191058">8824191058</a></p>
+        <p>LinkedIn: <a href="https://www.linkedin.com/in/ankursainians/" target="_blank">ankursainians</a></p>
+        <p>GitHub: <a href="https://github.com/ankursainians" target="_blank">AnkurSainiAns</a></p>
+    </div>
+`
